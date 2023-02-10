9596a6f3a1dae90d2d4910cb94b94b801a0d5790
RMNGarcia <renatomngarcia@gmail.com>
2023-02-10 15:35:40 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Put } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('app')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('test')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+
+}
